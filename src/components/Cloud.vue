56d367fffafab595a5414b2bca1c065d61ee68a9
Austin Hackett <ahackit.gmail.com>
2019-11-26 20:22:41 -0600

    size field

--- src/components/Cloud.vue
+++ src/components/Cloud.vue
@@ -2,7 +2,8 @@
   <div class="card">
     <div class="card-body">
       <h5 class="card-title">{{ cloud.cloud_type }}</h5>
-      <p class="card-text">{{ cloud.name }}</p>
+      <p class="card-text">Name: {{ cloud.name }}</p>
+	  <p class="card-text">Size: {{ cloud.size }}</p>
     </div>
   </div>
 </template>
