83b054957ccbd4ddea541d8ffffdde443c18dbd2
Austin Hackett <ahackit.gmail.com>
2019-11-26 20:24:24 -0600

    size

--- src/components/Cloud.vue
+++ src/components/Cloud.vue
@@ -3,7 +3,7 @@
     <div class="card-body">
       <h5 class="card-title">{{ cloud.cloud_type }}</h5>
       <p class="card-text">Name: {{ cloud.name }}</p>
-	  <p class="card-text">Size: {{ cloud.size }}</p>
+      <p class="card-text">Size: {{ cloud.size }}</p>
     </div>
   </div>
 </template>
